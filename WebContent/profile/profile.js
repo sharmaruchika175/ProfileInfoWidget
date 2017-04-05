e19f460a76a0695c0ad1b29889406c2aee9b5dd5
Ruchika Sharma <sharma.ruchika175@gmail.com>
2017-04-05 17:02:25 -0400

    Added functionality to Edit user

--- WebContent/profile/profile.js
+++ WebContent/profile/profile.js
@@ -4,6 +4,11 @@
 		firstname: "RUCHIKA", 
 		lastname: "SHARMA", 
 		followers: "12,345",
+		user	: {
+			email : "sharma.ruchika175@gmail.com",
+			address : "Brampton ON",
+			phone : "+1 9057830647"
+		},
 		statics : {
 			likes : "15,000",
 			comments : "7,500",
@@ -14,11 +19,19 @@
 	function initializeCache() {
 		$cache = {
 			bottonContainerMain		: $(".bottom-container.main"),
-			bottomContainerStats	: $(".bottom-container.statics")
+			bottomContainerStats	: $(".bottom-container.statics"),
+			bottomContainerUser		: $(".bottom-container.user"),
+			infoContainerStats		: $(".bottom-container .info-container.statics"),
+			infoContainerUser		: $(".bottom-container .info-container.user"),
+			editInfo 				: $(".edit-info"),
+			inputInfo				: $(".input-info"),
+			saveInfo				: $(".save-info"),
+			labelInfo 				: $(".label-info"),
+			
 		};
 	}
 	function initializeEvents() {
-		$(".bottom-container .info-container.statics").on('click', function (e) {
+		$cache.infoContainerStats.on('click', function (e) {
 			$cache.bottonContainerMain.hide();
 			$cache.bottomContainerStats.show();
 		});
@@ -28,7 +41,51 @@
 			$cache.bottomContainerStats.hide();
 		});
 
+		$cache.infoContainerUser.on('click', function (e) {
+			$cache.bottonContainerMain.hide();
+			$cache.bottomContainerUser.show();
+		});
+
+		$(".bottom-container.user .back-button").on('click', function (e) {
+			$cache.bottonContainerMain.show();
+			$cache.bottomContainerUser.hide();
+		});
 		
+		$cache.bottomContainerUser.find(".info-container").on('click', function(e){
+			$cache.bottomContainerUser.hide();
+			$cache.editInfo.show();
+			$cache.labelInfo.text($(this).find(".info-container-left").text());
+			var data = $(this).find(".info-container-right").text();
+			$cache.inputInfo.val(data);
+		});
+		
+		$cache.editInfo.find(".back-button").on('click', function (e) {
+			$cache.editInfo.hide();
+			$cache.bottomContainerUser.show();
+		});
+		
+		$cache.saveInfo.on('click', function (e){
+			var url = "www.dummyurl.com/getprofileinfo";
+			var label = $(this).closest(".edit-info").children(".label-info").text();
+			var data = label + ":" +  $cache.inputInfo.val();
+			console.log(data);
+			jQuery.ajax({
+				type	: "post",
+				url		: url,
+				data    : data,
+				success	: function(){
+					$cache.editInfo.hide();
+					$cache.bottomContainerUser.show();
+				},
+				error 	: function(){
+					$cache.editInfo.hide();
+					$cache.bottomContainerUser.show();
+				}
+			
+			});
+		});
+		
+
 	}
 
 	function loadWidget() {
@@ -37,7 +94,6 @@
 		jQuery.ajax({
 			type	: "get",
 			url		: url,
-			dataType: 'html',
 			success	: function(response){
 				//When data is retrieved from server, dummy profileInfoJson defined on top should be removed.
 				var profileInfoJson = response;
@@ -45,11 +101,14 @@
 					$(".user-details .firstname").text(profileInfoJson.firstname);
 					$(".user-details .lastname").text(profileInfoJson.lastname);
 					$(".user-followers span").text(profileInfoJson.followers);
-
-					$cache.bottomContainerStats.find(".followers").text(profileInfoJson.followers);
+					
 					$cache.bottomContainerStats.find(".likes").text(profileInfoJson.statics.likes);
 					$cache.bottomContainerStats.find(".comments").text(profileInfoJson.statics.comments);
 					$cache.bottomContainerStats.find(".posts").text(profileInfoJson.statics.posts);
+					
+					$cache.bottomContainerUser.find(".email").text(profileInfoJson.user.email);
+					$cache.bottomContainerUser.find(".address").text(profileInfoJson.user.address);
+					$cache.bottomContainerUser.find(".phone-number").text(profileInfoJson.user.phone);
 
 					$(".loader").hide();
 					$(".content").show();
@@ -62,10 +121,13 @@
 				$(".user-details .lastname").text(profileInfoJson.lastname);
 				$(".user-followers span").text(profileInfoJson.followers);
 
-				$cache.bottomContainerStats.find(".followers").text(profileInfoJson.followers);
 				$cache.bottomContainerStats.find(".likes").text(profileInfoJson.statics.likes);
 				$cache.bottomContainerStats.find(".comments").text(profileInfoJson.statics.comments);
 				$cache.bottomContainerStats.find(".posts").text(profileInfoJson.statics.posts);
+				
+				$cache.bottomContainerUser.find(".email").text(profileInfoJson.user.email);
+				$cache.bottomContainerUser.find(".address").text(profileInfoJson.user.address);
+				$cache.bottomContainerUser.find(".phone-number").text(profileInfoJson.user.phone);
 
 				$(".loader").hide();
 				$(".content").show();
